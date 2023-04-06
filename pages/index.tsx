d4d05e00e4e83ad783ac89e8797b11628e7e9c93
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2023-04-06 13:18:00 +1000

    final

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Billboard from "@/components/Billboard";
+import InfoModal from "@/components/InfoModal";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
 
 import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
@@ -25,12 +28,17 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
+
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
       <div className="h-[90vh]"></div>
     </>
